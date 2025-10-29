7c2bb04a4ca074938c6bc54344e334113d409b0f
Kyle Osterman <57078519+kylejosterman@users.noreply.github.com>
2025-10-29 13:18:49 -0400

    Revert "Fix lint (#1)"
    This reverts commit 6869e0d9d9c29a43f535dba9f993cf620c916a85.

--- Formula/agentsync.rb
+++ Formula/agentsync.rb
@@ -4,7 +4,7 @@ class Agentsync < Formula
   url "https://github.com/kylejosterman/agentsync/archive/refs/tags/0.1.0.tar.gz"
   sha256 "d0b9ae66502bc91d012b0fe54761fe1f5332f9e39bfa2ed9ea5282f33aa08886"
   license "MIT"
-  head "https://github.com/kylejosterman/agentsync.git", branch: "main"
+  head "https://github.com/kylejosterman/agentsync.git", branch: "main"  
 
   depends_on "rust" => :build
 
@@ -16,3 +16,4 @@ class Agentsync < Formula
     assert_match version.to_s, shell_output("#{bin}/agentsync --version")
   end
 end
+
