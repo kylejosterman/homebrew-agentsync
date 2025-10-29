fd1a0cc6bc4fdb19b13d1987b7ccab5f1d71cd79
Kyle Osterman <57078519+kylejosterman@users.noreply.github.com>
2025-10-29 17:32:57 +0000

    agentsync: add 0.1.0 bottle.

--- Formula/agentsync.rb
+++ Formula/agentsync.rb
@@ -6,6 +6,13 @@ class Agentsync < Formula
   license "MIT"
   head "https://github.com/kylejosterman/agentsync.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/kylejosterman/homebrew-agentsync/releases/download/agentsync-0.1.0"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "e0f30b0d05729be80e8e6c2722c4ecebe24c26ee783deedc175b0d97b528e17e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "329357d52a3faf8aa6476b32cff6557b90e77518a678af601c39929afab2fac6"
+  end
+
   depends_on "rust" => :build
 
   def install
