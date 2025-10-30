4c098de6e865e022fa7f4cbc9537758a8224c399
Kyle Osterman <57078519+kylejosterman@users.noreply.github.com>
2025-10-30 13:45:19 -0400

    Release 0.2.0
    Closes #4.
    
    Signed-off-by: Kyle Osterman <57078519+kylejosterman@users.noreply.github.com>

--- Formula/agentsync.rb
+++ Formula/agentsync.rb
@@ -1,13 +1,13 @@
 class Agentsync < Formula
   desc "CLI tool to synchronize AI agent rules for LLM platforms"
   homepage "https://github.com/kylejosterman/agentsync"
-  url "https://github.com/kylejosterman/agentsync/archive/refs/tags/0.1.0.tar.gz"
-  sha256 "d0b9ae66502bc91d012b0fe54761fe1f5332f9e39bfa2ed9ea5282f33aa08886"
+  url "https://github.com/kylejosterman/agentsync/archive/refs/tags/0.2.0.tar.gz"
+  sha256 "f56aa9dddf588117e3d79889583b80b032c2cb3d5288cc72b67c33bad583992a"
   license "MIT"
   head "https://github.com/kylejosterman/agentsync.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/kylejosterman/homebrew-agentsync/releases/download/agentsync-0.1.0"
+    root_url "https://github.com/kylejosterman/homebrew-agentsync/releases/download/agentsync-0.2.0"
     rebuild 1
     sha256 cellar: :any_skip_relocation, arm64_tahoe:  "e0f30b0d05729be80e8e6c2722c4ecebe24c26ee783deedc175b0d97b528e17e"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "329357d52a3faf8aa6476b32cff6557b90e77518a678af601c39929afab2fac6"
